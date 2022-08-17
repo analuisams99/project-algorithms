d54cf0c9b90eba2bd2e1613b8cdeb1f0d3c20261
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-17 19:12:14 -0300

    Cria função find_duplicate para encontrar números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    num_dict = dict()
+
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+
+        if num not in num_dict:
+            num_dict[num] = 1
+
+        else:
+            return num
+
+    return False
