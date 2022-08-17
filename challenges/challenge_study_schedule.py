bcb3162db34d5d535a05f12046ab60334601a173
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-17 16:16:35 -0300

    Cria função, study_schedule, que retorna o número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui."""
+    students_present = 0
+
+    for check_in, check_out in permanence_period:
+        try:
+            if check_in <= target_time <= check_out:
+                students_present += 1
+        except TypeError:
+            return None
+
+    return students_present
