2159b3f33e2799e81d53d2c22fbbdb79221a3a0c
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-17 18:56:49 -0300

    Cria função is_anagram para verificar se as palavras são anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def letter_count(word):
+    letter_dict = dict()
+
+    for letter in word.lower():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+
+    return letter_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = letter_count(first_string)
+    second = letter_count(second_string)
+
+    return True if first == second else False
